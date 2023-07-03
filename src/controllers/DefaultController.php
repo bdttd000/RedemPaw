d9d989e09c631d670b9d237a263314db4d04bb4d
Jakub Wartak <kuba428@op.pl>
2023-07-03 16:09:33 +0200

    pet object controller and logic

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -33,4 +33,10 @@ class DefaultController extends AppController
     {
         $this->render('privacyPolicy');
     }
+
+    public function addPet()
+    {
+
+        $this->render('addPet');
+    }
 }
